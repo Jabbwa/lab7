783c79e5541c9c1b3ecb7785238d5ca86de41a9c
Jabbwa <sopida_kit@cmu.ac.th>
2021-11-30 18:05:41 +0700

    complete

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,15 +1,28 @@
 #include<iostream>
 
 using namespace std;
-
+int adiff(int a, int b){
+    int x, y, c, result;
+    x=a%360;
+    y=b%360;
+    if (x>y)
+    {
+      c=x-y;
+    }else{
+      c=y-x;
+    }
+    if (c>180)
+    {
+        result=360-c;
+    }else{
+      result=c;
+    }
+    
+    return result;
+     
+}
 
 int main(){
-  cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
-  cout << adiff(1000,280);
   cout << adiff(60,244);
+
 }
